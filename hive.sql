f3ab9c1467639c056a67c1fed8157b49f27080b1
Randriamarosaina Sombiniaina Fitahiana <sombiniaina.randriamarosaina@outlook.fr>
2025-06-05 22:32:52 +0300

    Fix: Improve data integration for Oracle NoSQL and connect it to PostgreSQL hotel database

--- hive.sql
+++ hive.sql
@@ -1,21 +1,35 @@
-DROP TABLE IF EXISTS hotel_oraclenosql;
+DROP TABLE IF EXISTS hotel_postgres;
 
 DROP TABLE IF EXISTS hotel_review_oraclenosql;
 
-CREATE EXTERNAL TABLE  hotel_oraclenosql (hotel_id string, hotel_name string)
+CREATE EXTERNAL TABLE  hotel_review_oraclenosql (review_id string, hotel_id int, client_email string, rating int, comment string, review_date string)
 STORED BY 'oracle.kv.hadoop.hive.table.TableStorageHandler'
 TBLPROPERTIES (
   "oracle.kv.kvstore" = "kvstore",
   "oracle.kv.hosts" = "oraclenosql:5000", 
   "oracle.kv.hadoop.hosts" = "localhost/127.0.0.1", 
-  "oracle.kv.tableName" = "hotel"
+  "oracle.kv.tableName" = "review"
 );
 
-CREATE EXTERNAL TABLE  hotel_review_oraclenosql (hotel_id string, review_id string, client_name string, rating int, comment string, review_date string)
-STORED BY 'oracle.kv.hadoop.hive.table.TableStorageHandler'
+
+CREATE EXTERNAL TABLE hotel_postgres (
+  hotel_id INT,
+  nom STRING,
+  adresse STRING,
+  ville STRING,
+  pays STRING,
+  categorie INT,
+  nb_chambres INT,
+  prix_moyen FLOAT,
+  coordonnees_gps STRING,
+  date_creation TIMESTAMP
+)
+STORED BY 'org.apache.hive.storage.jdbc.JdbcStorageHandler'
 TBLPROPERTIES (
-  "oracle.kv.kvstore" = "kvstore",
-  "oracle.kv.hosts" = "oraclenosql:5000", 
-  "oracle.kv.hadoop.hosts" = "localhost/127.0.0.1", 
-  "oracle.kv.tableName" = "hotel.review"
+  "hive.sql.database.type" = "POSTGRES",
+  "hive.sql.jdbc.driver" = "org.postgresql.Driver",
+  "hive.sql.jdbc.url" = "jdbc:postgresql://hotel_postgres:5432/hotel_db",
+  "hive.sql.dbcp.username" = "hotel_admin",
+  "hive.sql.dbcp.password" = "hotel@123",
+  "hive.sql.table" = "hotels"
 );
